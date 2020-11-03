8d9f79574b08068a2371d00b7717e051e6a89c03
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-11-03 15:41:31 +0545

    Add Refresh Indicator

--- lib/pages/country.dart
+++ lib/pages/country.dart
@@ -18,6 +18,13 @@ class _CountryPageState extends State<CountryPage> {
     });
   }
 
+  Future fetchData() async {
+    setState(() {
+      this.countryData = null;
+    });
+    fetchStatisticData();
+  }
+
   @override
   void initState() {
     fetchStatisticData();
@@ -31,89 +38,92 @@ class _CountryPageState extends State<CountryPage> {
       appBar: AppBar(
         title: Text('Country Statistics'),
       ),
-      body: countryData == null
-          ? Center(
-              child: Column(
-              children: [
-                SizedBox(height: 50.0),
-                CircularProgressIndicator(),
-                SizedBox(height: 10.0),
-                Text(
-                  'fetching country data...',
-                  style: TextStyle(fontSize: 12),
-                )
-              ],
-            ))
-          : ListView.builder(
-              itemCount: countryData == null ? 0 : countryData.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  height: 130.0,
-                  margin:
-                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey[100],
-                        blurRadius: 10,
-                        offset: Offset(0, 10))
-                  ]),
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(countryData[index]['country'],
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Image.network(
-                              countryData[index]['countryInfo']['flag'],
-                              height: 50.0,
-                              width: 60.0,
-                            ),
-                          ],
+      body: RefreshIndicator(
+        onRefresh: fetchData,
+        child: countryData == null
+            ? Center(
+                child: Column(
+                children: [
+                  SizedBox(height: 50.0),
+                  CircularProgressIndicator(),
+                  SizedBox(height: 10.0),
+                  Text(
+                    'fetching country data...',
+                    style: TextStyle(fontSize: 12),
+                  )
+                ],
+              ))
+            : ListView.builder(
+                itemCount: countryData == null ? 0 : countryData.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 130.0,
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                    decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey[100],
+                          blurRadius: 10,
+                          offset: Offset(0, 10))
+                    ]),
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(countryData[index]['country'],
+                                  style: TextStyle(fontWeight: FontWeight.bold)),
+                              Image.network(
+                                countryData[index]['countryInfo']['flag'],
+                                height: 50.0,
+                                width: 60.0,
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Confirmed: ' +
-                                  countryData[index]['totalCases'].toString(),
-                              style: TextStyle(
-                                  color: Colors.cyan,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                                'Active: ' +
-                                    countryData[index]['activeCases']
-                                        .toString(),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Confirmed: ' +
+                                    countryData[index]['totalCases'].toString(),
                                 style: TextStyle(
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.bold)),
-                            Text(
-                                'Recovered: ' +
-                                    countryData[index]['totalRecovered']
-                                        .toString(),
-                                style: TextStyle(
-                                    color: Colors.green,
-                                    fontWeight: FontWeight.bold)),
-                            Text(
-                                'Deaths: ' +
-                                    countryData[index]['totalDeaths']
-                                        .toString(),
-                                style: TextStyle(
-                                    color: Colors.red,
-                                    fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              },
-            ),
+                                    color: Colors.cyan,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                  'Active: ' +
+                                      countryData[index]['activeCases']
+                                          .toString(),
+                                  style: TextStyle(
+                                      color: Colors.blue,
+                                      fontWeight: FontWeight.bold)),
+                              Text(
+                                  'Recovered: ' +
+                                      countryData[index]['totalRecovered']
+                                          .toString(),
+                                  style: TextStyle(
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.bold)),
+                              Text(
+                                  'Deaths: ' +
+                                      countryData[index]['totalDeaths']
+                                          .toString(),
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+      ),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -38,6 +38,17 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Future fetchData() async {
+    setState(() {
+      this.worldwideData = null;
+      this.nepalStatistics = null;
+      this.countryData = null;
+      this.news = null;
+      this.provinceData = null;
+    });
+    fetchStatisticData();
+  }
+
   @override
   void initState() {
     fetchStatisticData();
@@ -51,170 +62,175 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Quotes(),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Icon(Icons.public),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10),
+      body: RefreshIndicator(
+        onRefresh: fetchData,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Quotes(),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.public),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text(
+                            'Worldwide',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CountryPage()));
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5.0, horizontal: 10.0),
+                        decoration: BoxDecoration(
+                            color: primaryBlack,
+                            borderRadius: BorderRadius.circular(5.0)),
                         child: Text(
-                          'Worldwide',
+                          'Regional',
                           style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
                         ),
                       ),
-                    ],
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => CountryPage()));
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 5.0, horizontal: 10.0),
-                      decoration: BoxDecoration(
-                          color: primaryBlack,
-                          borderRadius: BorderRadius.circular(5.0)),
-                      child: Text(
-                        'Regional',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            worldwideData == null
-                ? Center(
-                    child: Column(
-                    children: [
-                      SizedBox(height: 10.0),
-                      CircularProgressIndicator(),
-                      SizedBox(height: 10.0),
-                      Text(
-                        'fetching worldwide data...',
-                        style: TextStyle(fontSize: 12),
-                      )
-                    ],
-                  ))
-                : WorldWidePanel(worldwideData: worldwideData),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Image.network(
-                        'https://disease.sh/assets/img/flags/np.png',
-                        height: 30.0,
-                        width: 30.0,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: Text(
-                          'Nepal',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ],
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => Nepal(
-                                  nepalStatistics: nepalStatistics,
-                                  provinceData: provinceData)));
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 4.0, horizontal: 8.0),
-                      decoration: BoxDecoration(
-                          color: primaryBlack,
-                          borderRadius: BorderRadius.circular(5.0)),
-                      child: Text(
-                        'View More',
-                        style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            nepalStatistics == null
-                ? Center(
-                    child: Column(
-                    children: [
-                      SizedBox(height: 10.0),
-                      CircularProgressIndicator(),
-                      SizedBox(height: 10.0),
-                      Text(
-                        'fetching Nepal data...',
-                        style: TextStyle(fontSize: 12),
-                      )
-                    ],
-                  ))
-                : NepalPanel(nepalStatistics: nepalStatistics),
-            SizedBox(height: 10.0),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: Text(
-                'Most affected Countries',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ),
-            countryData == null
-                ? Center(
-                    child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
+              worldwideData == null
+                  ? Center(
+                      child: Column(
                       children: [
                         SizedBox(height: 10.0),
-                        LinearProgressIndicator(),
+                        CircularProgressIndicator(),
                         SizedBox(height: 10.0),
                         Text(
-                          'fetching countrywise data...',
+                          'fetching worldwide data...',
                           style: TextStyle(fontSize: 12),
                         )
                       ],
+                    ))
+                  : WorldWidePanel(worldwideData: worldwideData),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Image.network(
+                          'https://disease.sh/assets/img/flags/np.png',
+                          height: 30.0,
+                          width: 30.0,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text(
+                            'Nepal',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
                     ),
-                  ))
-                : MostEffectCountry(countryData: countryData),
-            SizedBox(height: 20.0),
-            InfoPanel(news: news),
-            SizedBox(height: 20.0),
-            Center(
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Nepal(
+                                    nepalStatistics: nepalStatistics,
+                                    provinceData: provinceData)));
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 4.0, horizontal: 8.0),
+                        decoration: BoxDecoration(
+                            color: primaryBlack,
+                            borderRadius: BorderRadius.circular(5.0)),
+                        child: Text(
+                          'View More',
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              nepalStatistics == null
+                  ? Center(
+                      child: Column(
+                      children: [
+                        SizedBox(height: 10.0),
+                        CircularProgressIndicator(),
+                        SizedBox(height: 10.0),
+                        Text(
+                          'fetching Nepal data...',
+                          style: TextStyle(fontSize: 12),
+                        )
+                      ],
+                    ))
+                  : NepalPanel(nepalStatistics: nepalStatistics),
+              SizedBox(height: 10.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 10.0),
                 child: Text(
-              'WE ARE TOGETHER IN THE FIGHT',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.4,
-                  color: Colors.grey[800]),
-            )),
-            SizedBox(height: 20.0),
-          ],
+                  'Most affected Countries',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              countryData == null
+                  ? Center(
+                      child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 10.0),
+                          LinearProgressIndicator(),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'fetching countrywise data...',
+                            style: TextStyle(fontSize: 12),
+                          )
+                        ],
+                      ),
+                    ))
+                  : MostEffectCountry(countryData: countryData),
+              SizedBox(height: 20.0),
+              InfoPanel(news: news),
+              SizedBox(height: 20.0),
+              Center(
+                  child: Text(
+                'WE ARE TOGETHER IN THE FIGHT',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.4,
+                    color: Colors.grey[800]),
+              )),
+              SizedBox(height: 20.0),
+            ],
+          ),
         ),
       ),
     );
